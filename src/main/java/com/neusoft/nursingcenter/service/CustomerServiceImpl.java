996bddc3a68c2912cb7cf75a0cda771eab006561
Yrugit <577054257@qq.com>
2025-06-18 09:22:01 +0800

    客户管理部分实现

---
+++ src/main/java/com/neusoft/nursingcenter/service/CustomerServiceImpl.java
@@ -0,0 +1,9 @@
+package com.neusoft.nursingcenter.service;
+
+import com.neusoft.nursingcenter.mapper.CustomerMapper;
+
+public class CustomerServiceImpl implements CustomerService {
+	private CustomerMapper customerMapper;
+	
+	
+}
