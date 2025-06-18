9dd7b1a9e456238a60d1ebc4aef816349134198e
Yrugit <577054257@qq.com>
2025-06-18 14:44:47 +0800

    a

---
+++ src/main/java/com/neusoft/nursingcenter/mapper/MealItemMapper.java
@@ -0,0 +1,7 @@
+package com.neusoft.nursingcenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.neusoft.nursingcenter.entity.MealItem;
+
+public interface MealItemMapper extends BaseMapper<MealItem> {
+}
